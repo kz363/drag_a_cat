d389c055f1a95e2df513cd4f636ea8f6366a6fad
Apprentice <kz363@nyu.edu>
2014-06-20 10:44:18 -0400

    Fixed bug where mousemove was always running

--- public/js/application.js
+++ public/js/application.js
@@ -6,10 +6,16 @@ $(document).ready(function() {
     'width': $(window).width(),
     'height': $(window).height()
   });
+
   var start_x = 0;
   var start_y = 0;
+  var new_x = 0;
+  var new_y = 0;
+  var ready = true;
 
   div.on('mousedown', function(e) {
+    e.preventDefault();
+    ready = false;
     div.append('<div id="selection"></div>');
     start_x = e.pageX;
     start_y = e.pageY;
@@ -21,26 +27,29 @@ $(document).ready(function() {
     });
 
     div.on('mousemove', function(e) {
-      var move_x = e.pageX,
-      move_y = e.pageY,
-      width  = Math.abs(move_x - start_x),
-      height = Math.abs(move_y - start_y),
-      new_x, new_y;
+      e.preventDefault();
+      if(ready == false){
+        var moving_x = e.pageX;
+        var moving_y = e.pageY;
+        var width  = Math.abs(moving_x - start_x);
+        var height = Math.abs(moving_y - start_y);
+        console.log(moving_x);
+        console.log(moving_y);
+        new_x = (moving_x < start_x) ? (start_x - width) : start_x;
+        new_y = (moving_y < start_y) ? (start_y - height) : start_y;
 
-      new_x = (move_x < start_x) ? (start_x - width) : start_x;
-      new_y = (move_y < start_y) ? (start_y - height) : start_y;
-
-      $('#selection').css({
-        'width': width,
-        'height': height,
-        'top': new_y,
-        'left': new_x
+        $('#selection').css({
+          'width': width,
+          'height': height,
+          'top': new_y,
+          'left': new_x
       });
-      
-      div.on('mouseup', function(e) {
-        $('#cat img').remove();
-          small_x = start_x > new_x ? start_x : new_x;
-          small_y = start_y > new_y ? start_y : new_y;
+
+        div.on('mouseup', function(e) {
+          e.preventDefault();
+          $('#cat img').remove();
+          small_x = start_x < new_x ? start_x : new_x;
+          small_y = start_y < new_y ? start_y : new_y;
 
           cat.css({
             'width': width,
@@ -49,8 +58,10 @@ $(document).ready(function() {
             'left': new_x
           });
           cat.append("<img src=http://placekitten.com/"+width+"/"+height+">");
+          ready = true;
           $('#selection').remove();
         });
-    });
+      }});
+
 });
 });
